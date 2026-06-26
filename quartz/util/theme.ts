382d7a274955c969473b3877ed8b586006378e83
ikelyons <ike.lyons.ny@gmail.com>
2026-06-26 18:57:59 +0000

    More font changes (need to push to prod to get new fonts onto the server for local testing)

--- quartz/util/theme.ts
+++ quartz/util/theme.ts
@@ -67,7 +67,7 @@ function formatFontSpecification(
     features.push("ital")
   }
 
-  if (weights.length > 1) {
+  if (weights.length > 0) {
     const weightSpec = italic
       ? weights
           .flatMap((w) => [`0,${w}`, `1,${w}`])
